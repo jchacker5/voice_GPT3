005632a8c9f383052449de3bbdccc7d8a4e37f48
joe Defendre <jchacker5@gmail.com>
2023-03-04 14:44:34 -0500

    swtched to whisper for better dictation

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 api_key = "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"
+whisper_api_key = api_key
--- voice_GPT3.py
+++ voice_GPT3.py
@@ -2,77 +2,88 @@ import openai
 import pyttsx3
 import speech_recognition as sr
 import config
+import whisper
 
 # OpenAI API Key
-openai.api_key = config.api_key
+openai.api_secret_key = config.whisper_api_key
+# Set Whisper API Key
+whisper_api_key = config.whisper_api_key
+model = whisper.load_model("base")
 
-# Text to Speech
+# Set up the Whisper API client
+whisper = openai.api_client(api_key=config.whisper_api_key)
+whisper = openai.api_client(api_key=whisper_api_key)
+
+
+# intialize text to speech engine
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 
+
 def transcribe_audio_to_text(filename):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile() as source:
-       audio = recognizer.record(source)
-    try:
-        text = r.recognize_google(audio)
-        print("You said: ", text)
-        return text
-    except:
-        print("Sorry, I did not get that")
-        return ""
+    # Use whisper to transcribe the audio file
+    response = whisper.speech_to_text(
+        audio=open(filename, "rb"),
+        model="commercial",
+        language="en-US"
+    )
+    return response["text"]
+
 
 def generate_response(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-033",
+        engine="text-davinci-013",
         prompt=prompt,
-        temperature=0.9,
         max_tokens=4000,
-        n =1,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=["\n", " Human:", " AI:"]
+        n=1,
+        stop="\n",
+        temperature=0.9,
     )
-    return response.choices[0].text
+    return response["choices"][0]["text"]
 
-def speak(text):
+
+def speak_text(text):
     engine.say(text)
-    engine.runAndWait() 
+    engine.runAndWait()
+
 
 def main():
     while True:
-        # wait for user to say genius
-        r = sr.Recognizer()
+        # wait for user to say sarah
+        print("Say Sarah! to start recording")
+
         with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
             print("Say Sarah!")
-            audio = r.listen(source)
+            audio = recognizer.listen(source)
             try:
-                transcription = r.recognize_google(audio)
-                if transcription.lower == "Sarah":
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "sarah":
                     # record audio
                     filename = "input.wav"
-                    print("im listening Dre")
-                    with sr.microphone() as source:
+                    print("i'm listening Dre")
+                    speak_text("I'm listening Dre")
+                    with sr.Microphone() as source:
                         source.adjust_for_ambient_noise(source)
-                        source.pause_threshold = 1
-                        audio = r.listen(source, phrase_time_limit=5)
+                        source.pause_threshold = 2
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
                         with open(filename, "wb") as f:
                             f.write(audio.get_wav_data())
+
                     # Transcribe Audio to Text
-                    text = transcribe_audio_to_text(filename)
+                    text = model.transcribe(filename)
                     if text:
                         print("papi you said:", text)
 
                         # Generate Response using GPT-3
                         response = generate_response(text)
-                        print("GPT-3 says: ", response)
+                        print("GPT-3 says: {response}")
 
                         # read response using text to speech
-                        speak(response) 
+                        speak_text(response)
             except Exception as e:
-                print("an error occured: {}".format(e))
+                print("\n an error occured: {}".format(e))
+
 
 if __name__ == "__main__":
     main()
-                    
